7d5ce44ddc2dd6f3043e548864753c38717a5d1b
amitkuamr8486 <heyaku101@gmail.com>
2024-06-18 20:47:51 +0530

    feat:implement service discovery for user authentication and product catalog service

---
+++ src/test/java/com/amit/ecommerce/service_discovery/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.ecommerce.service_discovery.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
